f46a7d4b0eed71afb4c62631c1dbb5caa49fab61
Reorx <novoreorx@gmail.com>
2022-03-02 23:19:26 +0800

    complete basic functionalities

--- main.ts
+++ main.ts
@@ -1,25 +1,81 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { TFile, WorkspaceLeaf, ViewState, normalizePath } from "obsidian";
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PluginSettings {
+	endOfDayTime: string;
+	alwaysOpenNewTab: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PluginSettings = {
+	endOfDayTime: '05:00',
+	alwaysOpenNewTab: false,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const getTodayNotePath = (settings: PluginSettings, dailyNotesSettings: any) => {
+	let { folder, format } = dailyNotesSettings
+	if (!format) {
+		format = 'yyyy-MM-DD'
+	}
+	const splited = settings.endOfDayTime.split(':').map(Number)
+	const now = window.moment()
+	const shifted = now.subtract(splited[0], 'hours').subtract(splited[1], 'minutes')
+	console.log('now', now, 'shifted', shifted)
+
+	const todayPath = normalizePath(`${folder}/${shifted.format(format)}.md`);
+	return todayPath
+}
+
+const openTodayNoteInNewTab = async (app: App, settings: PluginSettings, dailyNotesSettings: any) => {
+	const todayPath = getTodayNotePath(settings, dailyNotesSettings)
+
+	if (settings.alwaysOpenNewTab) {
+		await openPathInNewTab(app, todayPath)
+		return
+	}
+
+	// try to find a existing tab
+	var todayNoteLeaf: WorkspaceLeaf
+	app.workspace.iterateAllLeaves((leaf: WorkspaceLeaf) => {
+		const { file } = leaf.view
+		if (file && file.path === todayPath) {
+			todayNoteLeaf = leaf
+			return
+		}
+	})
+
+	if (todayNoteLeaf) {
+		todayNoteLeaf.setViewState({
+			...todayNoteLeaf.getViewState(),
+		}, {focus: true})
+	} else {
+		await openPathInNewTab(app, todayPath)
+	}
+}
+
+const openPathInNewTab = async (app: App, path: string) => {
+	const file = app.vault.getAbstractFileByPath(path) as TFile
+	// console.log('openPathInNewTab', file)
+	await openFile(app, file, {
+		openInNewTab: true,
+		direction: NewTabDirection.vertical,
+		focus: true,
+		mode: FileViewMode.default,
+	})
+}
+
+export default class NewTabDailyPlugin extends Plugin {
+	settings: PluginSettings;
 
 	async onload() {
 		await this.loadSettings();
+		const dailyNotesSettings = this.app.internalPlugins.getPluginById('daily-notes').instance.options
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('calendar-with-checkmark', "Open today's daily note in new tab", async (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			await openTodayNoteInNewTab(this.app, this.settings, dailyNotesSettings);
+			new Notice("Today's daily note opened");
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -30,52 +86,15 @@ export default class MyPlugin extends Plugin {
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'open-today-daily-note-in-new-tab',
+			name: "Open today's daily note in new tab",
+			callback: async () => {
+				await openTodayNoteInNewTab(this.app, this.settings, dailyNotesSettings)
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new SettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -84,6 +103,11 @@ export default class MyPlugin extends Plugin {
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		/* TODO
+		- ensure the new tab is the only daily note opened
+		- day separator time
+		*/
+
 	}
 
 	async saveSettings() {
@@ -91,26 +115,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingTab extends PluginSettingTab {
+	plugin: NewTabDailyPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: NewTabDailyPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +128,59 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Daily notes new tab settings'});
+
+		const nowYMD = window.moment().format('yyyy-MM-DD')
+		const yesterdayYMD = window.moment().subtract(1, 'day').format('yyyy-MM-DD')
+		new Setting(containerEl)
+			.setName('End of day time')
+			.setDesc(`Determine today\'s date, if the value is 03:00 and the current datetime is ${nowYMD} 02:59, then the date for today is ${yesterdayYMD}`)
+			.addText(text => text
+				.setPlaceholder('HH:MM')
+				.setValue(this.plugin.settings.endOfDayTime)
+				.onChange(async (value) => {
+					this.plugin.settings.endOfDayTime = value;
+					await this.plugin.saveSettings();
+				}
+			));
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Always open new tab')
+			.setDesc(`Set true to always open new tab even if the daily note is already opened, otherwise the plugin will try to find the existing daily note and focus on it`)
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.alwaysOpenNewTab)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.alwaysOpenNewTab = value;
 					await this.plugin.saveSettings();
-				}));
+				}
+			));
+	}
+}
+
+enum FileViewMode {
+	source = 'source', preview = 'preview', default = 'default'
+}
+
+enum NewTabDirection {
+	vertical = "vertical", horizontal = "horizontal"
+}
+
+export async function openFile(app: App, file: TFile, optional?: {openInNewTab?: boolean, direction?: NewTabDirection, mode?: FileViewMode, focus?: boolean}) {
+	let leaf: WorkspaceLeaf;
+
+	if (optional?.openInNewTab && optional?.direction) {
+			leaf = app.workspace.splitActiveLeaf(optional.direction);
+	} else {
+			leaf = app.workspace.getUnpinnedLeaf();
+	}
+
+	await leaf.openFile(file)
+
+	if (optional?.mode || optional?.focus) {
+			await leaf.setViewState({
+					...leaf.getViewState(),
+					state: optional.mode && optional.mode !== 'default' ? {...leaf.view.getState(), mode: optional.mode} : leaf.view.getState(),
+					popstate: true,
+			} as ViewState, { focus: optional?.focus });
 	}
 }
